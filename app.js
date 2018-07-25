a34d8f48f6085f96e59cb3e86be7214332110bef
giqbal <gohars.iqbal@gmail.com>
2018-07-25 16:36:31 +0100

    Test deploy endpoint with heroku

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const cors = require('cors');
+const apiRouter = require('./routes/api');
+
+
+app.use(cors());
+
+app.use('/api/', apiRouter);
+
+app.use('*', (req, res, next) => {
+  next({ status: 404, message: 'Page not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: 'Internal Server Error' });
+});
+
+module.exports = app;
